1da8b817d4c0f77020cf737f3cdfb97728b99890
alexgm5555 <alexgm5555@hotmail.com>
2024-01-29 17:33:51 -0500

    fix: bug dashboar Company

--- src/app/pages/dashboard/company/page.tsx
+++ src/app/pages/dashboard/company/page.tsx
@@ -4,7 +4,6 @@ export default function company() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       hola desde dashboard
-      <Calendar></Calendar>
     </div>
   );
 }
